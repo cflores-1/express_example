f1355f2709ccbf0fd7d68350148f50402293c16d
Claudia Flores <cflores1@email.arizona.edu>
2022-01-30 13:13:55 -0700

    simple express example

---
+++ letsgo.js
@@ -0,0 +1,17 @@
+const express = require("express"); //allows us to use express, so we can access routes 
+const app = express();
+
+//Listen on port 8080
+app.listen(8080, () => {
+    console.log("server on!");
+});
+
+//this is going to be a web server. I'm using get to retrieve data
+//line 12 app.get when we hit enter we are sending a request
+//line 14 is the response that we get
+app.get("/claudias-favorite-movie", (req, res)=> {
+    res.send(`
+        <h1> Mrs. Doubtfire</h1>
+    `);
+})
+//^^ we just created a server! in just 16 lines of code!
